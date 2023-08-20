63166660a7b4a32fb907445e31adb71424d579d6
Glrdd Aye <138173113+ThatzPingu@users.noreply.github.com>
2023-08-20 11:25:48 -0700

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,4 @@
+@echo off
+title Pingu Cloner Installer - Developed by: pingu
+pip install -r requirements.txt
+start.bat
---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+title Pingu Cloner - Developed by: pingu
+python pingucloner.py
